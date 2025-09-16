22c068842d41e1478cef8e816dceccc5d38a6afd
darwinn <darwin.sibarani@ist.id>
2025-09-16 10:07:45 +0700

    Add endpoint test quarkus

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,27 @@
 package org.acme;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/quarkus")
 public class GreetingResource {
 
     @GET
+    @Path("example")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Quarkus REST";
     }
+
+    @POST
+    @Path("/test_time")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String testQuarkus() {
+        return "Darwin Coba Quarkus";
+    }
+
+
 }
